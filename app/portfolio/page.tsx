8d0498da5fdf57f2e8e3aa16a837532a76c71152
Abdelrahman <124844787+abdelrahman-mohamed8568@users.noreply.github.com>
2025-09-09 04:06:03 +0300

    Add Figma SVG icon to skills images

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import "../styles/portfolio.css";
 import Footer from "../components/footer/footer";
-import card1 from "@/public/images/cards/card1.jpg";
-import card2 from "@/public/images/cards/card2.jpg";
-import card3 from "@/public/images/cards/card3.jpg";
-import card4 from "@/public/images/cards/card4.jpg";
-import card5 from "@/public/images/cards/card5.jpg";
+import card1 from "@/public/images/cards/card1.png";
+import card2 from "@/public/images/cards/card2.png";
+import card3 from "@/public/images/cards/card3.png";
+import card4 from "@/public/images/cards/card4.png";
+import card5 from "@/public/images/cards/card5.png";
+import card6 from "@/public/images/cards/card6.png";
+import card7 from "@/public/images/cards/card7.png";
+import card8 from "@/public/images/cards/card8.png";
 import html from "@/public/images/skills/html.svg";
 import css from "@/public/images/skills/css.svg";
 import bootstrap from "@/public/images/skills/bootstrap.svg";
@@ -51,7 +54,7 @@ function Portfolio() {
         <div className="cardsContainer">
           <div className="portfolioCard">
             <div className="imgLink">
-              <Image src={card5} alt="" className="img" priority />
+              <Image src={card8} alt="" className="img" priority />
               <Link
                 href={"https://aurabeautyeg.com/"}
                 className="portfolioLink"
@@ -71,19 +74,73 @@ function Portfolio() {
                   />
                 </svg>
               </Link>
-              <Link
-                href={"https://github.com/abdelrahman-mohamed8568/aura-beauty"}
-                className="cardGit"
-                target="_blank"
-              >
-                GitHub
-              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={
+                    "https://github.com/abdelrahman-mohamed8568/aura-beauty"
+                  }
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://aurabeautyeg.com/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
             </div>
             <h4>Aura Beauty</h4>
           </div>
           <div className="portfolioCard">
             <div className="imgLink">
-              <Image src={card4} alt="" className="img" priority />
+              <Image src={card7} alt="" className="img" priority />
+              <Link
+                href={"https://five-palm-jumeirah.vercel.app/"}
+                className="portfolioLink"
+                target="_blank"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                  />
+                </svg>
+              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={
+                    "https://github.com/abdelrahman-mohamed8568/five-palm-jumeirah"
+                  }
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://five-palm-jumeirah.vercel.app/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
+            </div>
+            <h4>Five Palm Jumeirah</h4>
+          </div>
+          <div className="portfolioCard">
+            <div className="imgLink">
+              <Image src={card6} alt="" className="img" priority />
               <Link
                 href={"https://pop-movie-demo.netlify.app/"}
                 className="portfolioLink"
@@ -103,15 +160,106 @@ function Portfolio() {
                   />
                 </svg>
               </Link>
-              <Link
-                href={"https://github.com/abdelrahman-mohamed8568/Movie"}
-                className="cardGit"
-                target="_blank"
-              >
-                GitHub
-              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={"https://github.com/abdelrahman-mohamed8568/Movie"}
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://pop-movie-demo.netlify.app/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
             </div>
             <h4>Pop Movie</h4>
+          </div>
+          <div className="portfolioCard">
+            <div className="imgLink">
+              <Image src={card5} alt="" className="img" priority />
+              <Link
+                href={"https://meetus-rouge.vercel.app/"}
+                className="portfolioLink"
+                target="_blank"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                  />
+                </svg>
+              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={"https://github.com/abdelrahman-mohamed8568/meetus"}
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://meetus-rouge.vercel.app/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
+            </div>
+            <h4>Meetus</h4>
+          </div>
+          <div className="portfolioCard">
+            <div className="imgLink">
+              <Image src={card4} alt="" className="img" priority />
+              <Link
+                href={"https://furniro-rosy-one.vercel.app/"}
+                className="portfolioLink"
+                target="_blank"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                  />
+                </svg>
+              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={"https://github.com/abdelrahman-mohamed8568/furniro"}
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://furniro-rosy-one.vercel.app/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
+            </div>
+            <h4>Furniro</h4>
           </div>
           <div className="portfolioCard">
             <div className="imgLink">
@@ -135,13 +283,22 @@ function Portfolio() {
                   />
                 </svg>
               </Link>
-              <Link
-                href={"https://github.com/abdelrahman-mohamed8568/Z-Book"}
-                className="cardGit"
-                target="_blank"
-              >
-                GitHub
-              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={"https://github.com/abdelrahman-mohamed8568/Z-Book"}
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://z-book.netlify.app/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
             </div>
             <h4>Z Book</h4>
           </div>
@@ -167,13 +324,22 @@ function Portfolio() {
                   />
                 </svg>
               </Link>
-              <Link
-                href={"https://github.com/abdelrahman-mohamed8568/cruds"}
-                className="cardGit"
-                target="_blank"
-              >
-                GitHub
-              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={"https://github.com/abdelrahman-mohamed8568/cruds"}
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={"https://abdelrahman-mohamed8568.github.io/cruds/"}
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
             </div>
             <h4>Cards</h4>
           </div>
@@ -181,7 +347,9 @@ function Portfolio() {
             <div className="imgLink">
               <Image src={card1} alt="" className="img" priority />
               <Link
-                href={"https://abdelrahman-mohamed8568.github.io/Portfolio/"}
+                href={
+                  "https://abdelrahman-mohamed8568.github.io/Old-Portfolio/"
+                }
                 className="portfolioLink"
                 target="_blank"
               >
@@ -199,15 +367,26 @@ function Portfolio() {
                   />
                 </svg>
               </Link>
-              <Link
-                href={
-                  "https://github.com/abdelrahman-mohamed8568/Old-Portfolio"
-                }
-                className="cardGit"
-                target="_blank"
-              >
-                GitHub
-              </Link>
+              <div className="cardInfo">
+                <Link
+                  href={
+                    "https://github.com/abdelrahman-mohamed8568/Old-Portfolio"
+                  }
+                  className="cardGit"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+                <Link
+                  href={
+                    "https://abdelrahman-mohamed8568.github.io/Old-Portfolio/"
+                  }
+                  className="cardLive"
+                  target="_blank"
+                >
+                  Live
+                </Link>
+              </div>
             </div>
             <h4>old portfolio</h4>
           </div>
